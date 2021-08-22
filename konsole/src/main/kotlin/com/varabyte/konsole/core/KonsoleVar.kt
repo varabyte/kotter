dc2223d3edb28c1b439fe28315c9908f24f42e5f
David Herman <bit.spittle@gmail.com>
2021-08-22 10:44:30 -0700

    Update KonsoleVar to be associated with a block
    This way, you won't have old KonsoleVars triggered refreshes in
    unrelated blocks by accident later.

--- konsole/src/main/kotlin/com/varabyte/konsole/core/KonsoleVar.kt
+++ konsole/src/main/kotlin/com/varabyte/konsole/core/KonsoleVar.kt
@@ -1,13 +1,48 @@
 package com.varabyte.konsole.core
 
+import java.lang.ref.WeakReference
 import kotlin.reflect.KProperty
 
+/**
+ * A special variable which can be used to auto-rerender a target [KonsoleBlock] without needing to call
+ * [RunUntilScope.rerender] yourself.
+ *
+ * The way it works is, when this variable is fetched, it is checked if this has happened while we're in an active
+ * block:
+ *
+ * ```
+ * var count by KonsoleVar(0)
+ * konsole { <-- active block
+ *    for (i in 0 until count) { // <-- getValue happens here, gets associated with active block
+ *      text("*")
+ *    }
+ * }.runUntilFinished {
+ *   while (count < 5) {
+ *     delay(1000)
+ *     ++count // <-- setValue happens here, causes active block to rerun
+ *   }
+ * }
+ *
+ * count = 123 // Setting count out of a konsole block is fine; nothing is triggered
+ * ```
+ */
 class KonsoleVar<T>(private var value: T) {
-    operator fun getValue(thisRef: Any?, prop: KProperty<*>): T = value
+    private var associatedBlockRef: WeakReference<KonsoleBlock>? = null
+    operator fun getValue(thisRef: Any?, prop: KProperty<*>): T {
+        associatedBlockRef = KonsoleBlock.active?.let { WeakReference(it) }
+        return value
+    }
     operator fun setValue(thisRef: Any?, prop: KProperty<*>, value: T) {
         if (this.value != value) {
             this.value = value
-            KonsoleBlock.active?.requestRerender()
+            associatedBlockRef?.get()?.let { associatedBlock ->
+                KonsoleBlock.active?.let { activeBlock ->
+                    if (associatedBlock === activeBlock) activeBlock.requestRerender()
+                } ?: run {
+                    // Our old block is finished, no need to keep a reference around to it anymore.
+                    associatedBlockRef = null
+                }
+            }
         }
     }
 }
