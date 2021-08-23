56019a15fedeff0728f0b32d9579a88307e0e72d
David Herman <bit.spittle@gmail.com>
2021-08-22 20:35:35 -0700

    Add application plugin to the Konsole example

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.5.10"
+    application
 }
 
 group = "com.varabyte"
@@ -13,4 +14,8 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(project(":konsole"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
+}
+
+application {
+    mainClass.set("MainKt")
 }
