e00f23a5464c09e93b114a692a59519edd390a1a
David Herman <bit.spittle@gmail.com>
2021-08-27 09:48:30 -0700

    Hide the system terminal cursor
    We have our own custom cursor handling, so we remove the terminal one to
    avoid confusion.

--- konsole/src/main/kotlin/com/varabyte/konsole/terminal/Terminal.kt
+++ konsole/src/main/kotlin/com/varabyte/konsole/terminal/Terminal.kt
@@ -3,6 +3,7 @@ package com.varabyte.konsole.terminal
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import org.jline.terminal.TerminalBuilder
+import org.jline.utils.InfoCmp
 import java.io.IOException
 
 /**
@@ -17,6 +18,7 @@ interface Terminal : AutoCloseable {
  * A class which interacts directly with the underlying system terminal, e.g. println
  */
 class SystemTerminal : Terminal {
+    private lateinit var previousCursorSetting: InfoCmp.Capability
     private val terminal = TerminalBuilder.builder()
         .system(true)
         // Don't use JLine's virtual terminal - use ours! Because this is false, this builder will throw an exception
@@ -26,6 +28,15 @@ class SystemTerminal : Terminal {
         .build().apply {
             // Swallow keypresses - instead, Konsole will handle them
             enterRawMode()
+
+            // Hide the cursor; we'll handle it ourselves
+            val restoreCursorCapabilities = listOf(
+                InfoCmp.Capability.cursor_normal,
+                InfoCmp.Capability.cursor_visible,
+            )
+            previousCursorSetting = restoreCursorCapabilities
+                .firstOrNull { c -> getBooleanCapability(c) } ?: InfoCmp.Capability.cursor_normal
+            puts(InfoCmp.Capability.cursor_invisible)
         }
 
     override fun write(text: String) {
@@ -46,6 +57,9 @@ class SystemTerminal : Terminal {
     }
 
     override fun close() {
+        terminal.puts(previousCursorSetting)
+        terminal.flush()
+
         terminal.close()
     }
 }
