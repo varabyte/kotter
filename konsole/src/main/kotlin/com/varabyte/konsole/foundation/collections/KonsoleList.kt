adc301c2d06234b96d6f18324020487e31e6f535
David Herman <bit.spittle@gmail.com>
2021-08-28 16:07:18 -0700

    Fix busted KonsoleList get/set logic
    Before, it might have caused unecessary rerenders.

--- konsole/src/main/kotlin/com/varabyte/konsole/foundation/collections/KonsoleList.kt
+++ konsole/src/main/kotlin/com/varabyte/konsole/foundation/collections/KonsoleList.kt
@@ -27,105 +27,67 @@ import kotlin.concurrent.withLock
  */
 @ThreadSafe
 class KonsoleList<T> internal constructor(app: KonsoleApp, vararg elements: T) : MutableList<T> {
+    // KonsoleVar already has a lot of nice logic for updating the render block as necessary, so we delegate to it to
+    // avoid reimplementing the logic here
+    private var modifyCountVar by app.konsoleVarOf(0)
+    private var modifyCount = 0
+
     private val lock = ReentrantLock()
     @GuardedBy("lock")
     private val delegateList = mutableListOf(*elements)
-    // Lazy - just create an additional var which handles all the fancy konsole rerendering logic
-    private var modifyCount by app.konsoleVarOf(0)
+
+    private fun <R> readLock(block: () -> R): R {
+        return lock.withLock {
+            // Triggers KonsoleVar.getValue but not setValue (which, here, aborts early because value is the same)
+            modifyCountVar = modifyCountVar
+            block()
+        }
+    }
+
+    private fun <R> writeLock(block: () -> R): R {
+        return lock.withLock {
+            // Triggers KonsoleVar.setValue but not getValue
+            modifyCountVar = ++modifyCount
+            block()
+        }
+    }
 
     /**
      * Allow calls to lock the list for a longer time than just a single field at a time, useful for example if
-     * modifying many fields at once
+     * reading or updating many fields at once.
      *
      * @param R The result type of any value produced as a side effect of calling [block]; can be `Unit`
      */
     fun <R> withLock(block: KonsoleList<T>.() -> R): R = lock.withLock { this.block() }
 
     // Immutable functions
-    override val size: Int get() = lock.withLock { delegateList.size }
-    override fun contains(element: T) = lock.withLock { delegateList.contains(element) }
-    override fun containsAll(elements: Collection<T>) = lock.withLock { delegateList.containsAll(elements) }
-    override fun get(index: Int): T = lock.withLock { delegateList[index] }
-    override fun indexOf(element: T) = lock.withLock { delegateList.indexOf(element) }
-    override fun isEmpty() = lock.withLock { delegateList.isEmpty() }
-    override fun lastIndexOf(element: T) = lock.withLock { delegateList.lastIndexOf(element) }
-    override fun subList(fromIndex: Int, toIndex: Int): MutableList<T> = lock.withLock { delegateList.subList(fromIndex, toIndex) }
+    override val size: Int get() = readLock { delegateList.size }
+    override fun contains(element: T) = readLock { delegateList.contains(element) }
+    override fun containsAll(elements: Collection<T>) = readLock { delegateList.containsAll(elements) }
+    override fun get(index: Int): T = readLock { delegateList[index] }
+    override fun indexOf(element: T) = readLock { delegateList.indexOf(element) }
+    override fun isEmpty() = readLock { delegateList.isEmpty() }
+    override fun lastIndexOf(element: T) = readLock { delegateList.lastIndexOf(element) }
+    override fun subList(fromIndex: Int, toIndex: Int): MutableList<T> = readLock { delegateList.subList(fromIndex, toIndex) }
 
     // Iterators
     // Note: We return iterators to the copy of a list, so we don't have to worry about another thread clobbering it
-
-    override fun iterator(): MutableIterator<T> {
-        return lock.withLock { delegateList.toMutableList() }.iterator()
-    }
-
-    override fun listIterator(): MutableListIterator<T> {
-        return lock.withLock { delegateList.toMutableList() }.listIterator()
-    }
-
-    override fun listIterator(index: Int): MutableListIterator<T> {
-        return lock.withLock { delegateList.toMutableList() }.listIterator(index)
-    }
+    override fun iterator(): MutableIterator<T> = readLock { delegateList.toMutableList() }.iterator()
+    override fun listIterator(): MutableListIterator<T> = readLock { delegateList.toMutableList() }.listIterator()
+    override fun listIterator(index: Int): MutableListIterator<T> = readLock { delegateList.toMutableList() }.listIterator(index)
 
     // Mutable methods
-    override fun add(element: T): Boolean {
-        return lock.withLock {
-            delegateList.add(element).also { modifyCount++ }
-        }
-    }
-
-    override fun add(index: Int, element: T) {
-        return lock.withLock {
-            delegateList.add(index, element).also { modifyCount++ }
-        }
-    }
-
-    override fun addAll(index: Int, elements: Collection<T>): Boolean {
-        return lock.withLock {
-            delegateList.addAll(index, elements).also { modifyCount++ }
-        }
-    }
-
-    override fun addAll(elements: Collection<T>): Boolean {
-        return lock.withLock {
-            delegateList.addAll(elements).also { modifyCount++ }
-        }
-    }
-
-    override fun clear() {
-        return lock.withLock {
-            delegateList.clear().also { modifyCount++ }
-        }
-    }
-
-    override fun remove(element: T): Boolean {
-        return lock.withLock {
-            delegateList.remove(element).also { modifyCount++ }
-        }
-    }
-
-    override fun removeAll(elements: Collection<T>): Boolean {
-        return lock.withLock {
-            delegateList.removeAll(elements).also { modifyCount++ }
-        }
-    }
-
-    override fun removeAt(index: Int): T {
-        return lock.withLock {
-            delegateList.removeAt(index).also { modifyCount++ }
-        }
-    }
-
-    override fun retainAll(elements: Collection<T>): Boolean {
-        return lock.withLock {
-            delegateList.retainAll(elements).also { modifyCount++ }
-        }
-    }
-
-    override fun set(index: Int, element: T): T {
-        return lock.withLock {
-            delegateList.set(index, element).also { modifyCount++ }
-        }
-    }
+    override fun add(element: T): Boolean = writeLock { delegateList.add(element) }
+    override fun add(index: Int, element: T) = writeLock { delegateList.add(index, element) }
+    override fun addAll(index: Int, elements: Collection<T>): Boolean =
+        writeLock { delegateList.addAll(index, elements) }
+    override fun addAll(elements: Collection<T>): Boolean = writeLock { delegateList.addAll(elements) }
+    override fun clear() = writeLock { delegateList.clear() }
+    override fun remove(element: T): Boolean = writeLock { delegateList.remove(element) }
+    override fun removeAll(elements: Collection<T>): Boolean = writeLock { delegateList.removeAll(elements) }
+    override fun removeAt(index: Int): T = writeLock { delegateList.removeAt(index) }
+    override fun retainAll(elements: Collection<T>): Boolean = writeLock { delegateList.retainAll(elements) }
+    override fun set(index: Int, element: T): T = writeLock { delegateList.set(index, element) }
 }
 
 /** Create a [KonsoleList] whose scope is tied to this app. */
