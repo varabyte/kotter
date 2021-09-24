ee9e91c07c1bff1debe2f169ad94988b447ce9f6
David Herman <bit.spittle@gmail.com>
2021-09-24 10:41:46 -0700

    Add source compatibility to Konsole
    Otherwise we end up generating jars that (I think?) are tied to the
    version of the JDK it is built with.

--- konsole/build.gradle.kts
+++ konsole/build.gradle.kts
@@ -56,6 +56,8 @@ dependencies {
 java {
     withJavadocJar()
     withSourcesJar()
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 publishing {
