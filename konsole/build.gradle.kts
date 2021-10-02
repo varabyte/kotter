bf6f100a35c21f14e509410d2b4d50abd1b793e0
David Herman <bitspittle@gmail.com>
2021-10-02 16:39:35 -0700

    Tag Konsole at v0.9.0

--- konsole/build.gradle.kts
+++ konsole/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.varabyte.konsole"
-version = "0.9.0-SNAPSHOT"
+version = "0.9.0"
 
 fun shouldSign() = (findProperty("konsole.sign") as? String).toBoolean()
 fun shouldPublishToGCloud(): Boolean {
