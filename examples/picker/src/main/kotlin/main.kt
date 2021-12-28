e4e64a3d856bccc15cb1c916f6b3aeb05700cb5e
David Herman <bitspittle@gmail.com>
2021-12-28 11:32:11 -0800

    Picker sample: Allow entering saturation / value also

--- examples/picker/src/main/kotlin/main.kt
+++ examples/picker/src/main/kotlin/main.kt
@@ -2,11 +2,12 @@ import com.varabyte.kotter.foundation.input.Keys
 import com.varabyte.kotter.foundation.input.input
 import com.varabyte.kotter.foundation.input.onInputEntered
 import com.varabyte.kotter.foundation.input.onKeyPressed
-import com.varabyte.kotter.foundation.input.runUntilKeyPressed
-import com.varabyte.kotter.foundation.session
 import com.varabyte.kotter.foundation.liveVarOf
+import com.varabyte.kotter.foundation.runUntilSignal
+import com.varabyte.kotter.foundation.session
 import com.varabyte.kotter.foundation.text.HSV
 import com.varabyte.kotter.foundation.text.hsv
+import com.varabyte.kotter.foundation.text.p
 import com.varabyte.kotter.foundation.text.text
 import com.varabyte.kotter.foundation.text.textLine
 import kotlin.math.roundToInt
@@ -17,25 +18,37 @@ const val H = 15
 private fun Int.toHexStr() = this.toString(16).padStart(2, '0')
 private fun Float.toPercentStr() = "${(this * 100).roundToInt()}%"
 
+enum class State {
+    PICKING_COLOR,
+    ENTERING_HUE,
+    ENTERING_SAT,
+    ENTERING_VAL,
+}
+
 fun main() = session {
     // Instructions never need to change; output them first
     section {
-        textLine()
-        textLine("h: enter hue")
-        textLine("arrows: move picker (changes saturation and value)")
-        textLine("home/end: saturation to 0/100%")
-        textLine("pg up/down: value to 0/100%")
-        textLine("Press Q to quit")
-        textLine()
-        textLine("NOTE: If colors look chunky, your terminal may not support true colors")
-        textLine()
+        p {
+            textLine("h: enter hue")
+            textLine("s: enter saturation")
+            textLine("v: enter value")
+            textLine()
+            textLine("arrows: move picker (changes saturation and value)")
+            textLine("home/end: saturation to 0/100%")
+            textLine("pg up/down: value to 0/100%")
+            textLine()
+            textLine("Press Q to quit")
+            textLine()
+            textLine("NOTE: If colors look clumped, your terminal may not support true colors")
+            textLine()
+        }
     }.run()
 
     // Instructions never need to change; output them first
     var xCurr by liveVarOf(0)
     var yCurr by liveVarOf(0)
     var h by liveVarOf(0)
-    var enteringHue by liveVarOf(false)
+    var state by liveVarOf(State.PICKING_COLOR)
     section {
         val sCurr = (xCurr / W.toFloat())
         val vCurr = 1f - (yCurr / H.toFloat())
@@ -59,17 +72,19 @@ fun main() = session {
 
         val rgb = HSV(h, sCurr, vCurr).toRgb()
         textLine()
-        if (enteringHue) {
-            text("Enter hue (0 - 360): "); input(); textLine()
-        }
-        else {
-            textLine("HSV: $h°, ${sCurr.toPercentStr()}, ${vCurr.toPercentStr()}")
-            textLine("RGB: ${rgb.r}, ${rgb.g}, ${rgb.b} (0x${rgb.r.toHexStr()}${rgb.g.toHexStr()}${rgb.b.toHexStr()})")
+        when (state) {
+            State.ENTERING_HUE -> { text("Enter hue (0 - 360): "); input(); textLine() }
+            State.ENTERING_SAT -> { text("Enter saturation (0 - 100): "); input(); textLine() }
+            State.ENTERING_VAL -> { text("Enter value (0 - 100): "); input(); textLine() }
+            else -> {
+                textLine("HSV: $h°, ${sCurr.toPercentStr()}, ${vCurr.toPercentStr()}")
+                textLine("RGB: ${rgb.r}, ${rgb.g}, ${rgb.b} (0x${rgb.r.toHexStr()}${rgb.g.toHexStr()}${rgb.b.toHexStr()})")
+            }
         }
         textLine()
-    }.runUntilKeyPressed(Keys.Q) {
+    }.runUntilSignal {
         onKeyPressed {
-            if (!enteringHue) {
+            if (state == State.PICKING_COLOR) {
                 when (key) {
                     Keys.LEFT -> xCurr = (xCurr - 1).coerceAtLeast(0)
                     Keys.RIGHT -> xCurr = (xCurr + 1).coerceAtMost(W)
@@ -79,18 +94,46 @@ fun main() = session {
                     Keys.END -> xCurr = W
                     Keys.PAGE_UP -> yCurr = 0
                     Keys.PAGE_DOWN -> yCurr = H
-                    Keys.H -> enteringHue = true
+                    Keys.H -> state = State.ENTERING_HUE
+                    Keys.S -> state = State.ENTERING_SAT
+                    Keys.V -> state = State.ENTERING_VAL
+                    Keys.Q -> signal()
+                }
+            }
+            else {
+                if (key == Keys.ESC) {
+                    state = State.PICKING_COLOR
                 }
             }
         }
 
         onInputEntered {
-            enteringHue = false
-            input.toIntOrNull()?.let { hIn ->
-                if (hIn in (0 .. 360)) {
-                    h = hIn
+            when (state) {
+                State.ENTERING_HUE -> {
+                    input.toIntOrNull()?.let { hIn ->
+                        if (hIn in (0..360)) {
+                            h = hIn
+                        }
+                    }
                 }
+                State.ENTERING_SAT -> {
+                    input.toIntOrNull()?.let { sIn ->
+                        if (sIn in (0..100)) {
+                            xCurr = W * sIn / 100
+                        }
+                    }
+                }
+                State.ENTERING_VAL -> {
+                    input.toIntOrNull()?.let { vIn ->
+                        if (vIn in (0..100)) {
+                            yCurr = H - H * vIn / 100
+                        }
+                    }
+                }
+                else -> { error("Unexpected state: $state") }
             }
+
+            state = State.PICKING_COLOR
         }
     }
 }
