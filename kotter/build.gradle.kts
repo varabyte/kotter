210cb8b709a8caab4c8b92a3d3efaa31bfa46db2
David Herman <bitspittle@gmail.com>
2021-12-28 16:43:19 -0800

    Begin work on v0.9.5

--- kotter/build.gradle.kts
+++ kotter/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.varabyte.kotter"
-version = "0.9.4"
+version = "0.9.5-SNAPSHOT"
 
 fun shouldSign() = (findProperty("kotter.sign") as? String).toBoolean()
 fun shouldPublishToGCloud(): Boolean {
