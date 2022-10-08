5b07568d4d6b6d8ef0924b3d5e13dac9f218b6a6
David Herman <bitspittle@gmail.com>
2022-10-07 20:15:51 -0700

    Nit: Make a better header comment

--- kotter/src/test/kotlin/com/varabyte/kotter/foundation/TestSession.kt
+++ kotter/src/test/kotlin/com/varabyte/kotter/foundation/TestSession.kt
@@ -4,10 +4,9 @@ import com.varabyte.kotter.runtime.Session
 import com.varabyte.kotter.terminal.TestTerminal
 
 /**
- * Like a [session] block but provides a [TestTerminal] as a lambda argument.
+ * Like a [session] block but backed by a [TestTerminal], which is provides as a lambda argument.
  *
- * The [TestTerminal] backs this session and provides a useful way to assert that various
- * Kotter commands resulted in expected behavior.
+ * The [TestTerminal] is a useful way to assert that various Kotter commands resulted in expected behavior.
  */
 fun testSession(block: Session.(TestTerminal) -> Unit) {
     val terminal = TestTerminal()
