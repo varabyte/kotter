951c65058458258166acbb3bc1a33884f0ea8a17
David Herman <bitspittle@gmail.com>
2023-02-20 16:30:22 -0800

    Re-set JVM compatibility back to JDK1.8
    Looks like this got bumped up due to some experimentation with
    Compose that never got dropped back down again.

--- kotter/build.gradle.kts
+++ kotter/build.gradle.kts
@@ -73,8 +73,8 @@ dependencies {
 java {
     withJavadocJar()
     withSourcesJar()
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks.withType<KotlinCompile>().configureEach {
