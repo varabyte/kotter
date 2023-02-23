9eef1c340a682a509b6240a80e4ab415f9fc6d3f
David Herman <bitspittle@gmail.com>
2023-02-22 17:32:26 -0800

    Add the Gradle plugin for publishing to the nexus repository manager

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,24 @@
 // Note: This seems to need to exist here and NOT in settings.gradle.kts pluginManagment, or else Gradle/Kotlin whines
 // at build about the same plugin being loaded multiple times.
 // See also: https://docs.gradle.org/current/userguide/plugins.html#sec:subprojects_plugins_dsl
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlinx.kover) apply false
     alias(libs.plugins.jetbrains.dokka) apply false
+    alias(libs.plugins.nexus.publish)
+}
+
+group = "com.varabyte.kotter"
+version = libs.versions.kotter.get()
+
+nexusPublishing {
+    repositories {
+        sonatype {  //only for users registered in Sonatype after 24 Feb 2021
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            (findProperty("ossrhUsername") as? String)?.let { username.set(it) }
+            (findProperty("ossrhPassword") as? String)?.let { password.set(it) }
+        }
+    }
 }
