a2ff0f614e595fa1d4dc4cae6ea445e2b2cceb9f
David Herman <bitspittle@gmail.com>
2023-02-28 16:47:22 -0800

    Update how the native example recommends listing targets

--- examples/native/build.gradle.kts
+++ examples/native/build.gradle.kts
@@ -12,30 +12,35 @@ repositories {
 }
 
 kotlin {
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
-
-    nativeTarget.apply {
-        binaries {
-            executable {
-                entryPoint = "main"
+    // List of supported binary targets
+    // Note: You will need the right machine to build each one; otherwise, the target is disabled automatically
+    listOf(
+        linuxX64(), // Linux
+        mingwX64(), // Windows
+        macosArm64(), // Mac M1
+        macosX64(), // Mac Legacy
+    ).forEach { nativeTarget ->
+        nativeTarget.apply {
+            binaries {
+                executable {
+                    entryPoint = "main"
+                }
             }
         }
     }
 
     sourceSets {
-        val nativeMain by getting {
+        val nativeMain by creating {
             dependencies {
                 implementation(project(":kotter"))
                 implementation(libs.kotlinx.coroutines)
             }
         }
+
+        val linuxX64Main by getting { dependsOn(nativeMain) }
+        val mingwX64Main by getting { dependsOn(nativeMain) }
+        val macosArm64Main by getting { dependsOn(nativeMain) }
+        val macosX64Main by getting { dependsOn(nativeMain) }
     }
 }
 
