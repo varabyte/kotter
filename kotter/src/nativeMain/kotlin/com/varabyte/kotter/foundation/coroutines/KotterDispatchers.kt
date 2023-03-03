5ba2ca16133a7b37a52188f1bc9fbd7d4b4b99dc
David Herman <bitspittle@gmail.com>
2023-03-03 00:24:19 -0800

    Change native render dispatcher to fixed pool context
    ... because otherwise I would get a race condition, not sure why
    to be honest.

--- kotter/src/nativeMain/kotlin/com/varabyte/kotter/foundation/coroutines/KotterDispatchers.kt
+++ kotter/src/nativeMain/kotlin/com/varabyte/kotter/foundation/coroutines/KotterDispatchers.kt
@@ -1,12 +1,8 @@
 package com.varabyte.kotter.foundation.coroutines
 
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.*
 
-@OptIn(ExperimentalCoroutinesApi::class)
 actual object KotterDispatchers {
-    internal actual val Render: CoroutineDispatcher = newSingleThreadContext("Kotter Render")
+    internal actual val Render: CoroutineDispatcher = newFixedThreadPoolContext(1, "Render Thread")
     actual val IO: CoroutineDispatcher = Dispatchers.Default
 }
