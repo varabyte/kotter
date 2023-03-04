307dfb60edfa38ff0811070d6dc363334d3c138b
David Herman <bitspittle@gmail.com>
2023-03-03 16:20:05 -0800

    Remove varabyte-repos repository
    After pushing truth to maven, it's no longer necessary

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,6 @@ nexusPublishing {
 allprojects {
     repositories {
         mavenCentral()
-        maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
     }
 
     tasks.withType<JavaCompile>().configureEach {
