075ba30c396d5bc7005e6b514fc933025f89806b
David Herman <bitspittle@gmail.com>
2023-03-04 09:46:22 -0800

    Fix case where ReentrantReadWriteLock could deadlock

--- kotter/src/commonMain/kotlin/com/varabyte/kotter/platform/concurrent/Thread.kt
+++ kotter/src/commonMain/kotlin/com/varabyte/kotter/platform/concurrent/Thread.kt
@@ -1,8 +1,10 @@
 package com.varabyte.kotter.platform.concurrent
 
+internal expect class ThreadId
+
 internal expect class Thread {
     companion object {
-        fun getId(): Any
+        fun getId(): ThreadId
         fun sleepMs(millis: Int)
     }
 }
