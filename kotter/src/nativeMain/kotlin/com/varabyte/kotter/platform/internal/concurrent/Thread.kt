d36805ea96e0b974303b9f54916e5c2e0f7d14d4
David Herman <bitspittle@gmail.com>
2024-10-02 10:27:36 -0700

    Nit: Remove unecessary FQN
    (Probably leftover from some automatic refactor)

--- kotter/src/nativeMain/kotlin/com/varabyte/kotter/platform/internal/concurrent/Thread.kt
+++ kotter/src/nativeMain/kotlin/com/varabyte/kotter/platform/internal/concurrent/Thread.kt
@@ -16,7 +16,7 @@ internal actual class ThreadId(private val pthread: pthread_t) {
 internal actual class Thread {
     actual companion object {
         @Suppress("UNNECESSARY_NOT_NULL_ASSERTION") // Assertion needed when compiling on Mac
-        actual fun getId(): ThreadId = com.varabyte.kotter.platform.internal.concurrent.ThreadId(pthread_self()!!)
+        actual fun getId(): ThreadId = ThreadId(pthread_self()!!)
         actual fun sleepMs(millis: Int) {
             usleep((millis * 1000).toUInt())
         }
