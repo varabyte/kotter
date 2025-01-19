b10ff20b3e620ab62999c3af2350727f886b19ff
David Herman <bitspittle@gmail.com>
2025-01-18 23:22:17 -0800

    Make Gradle scripts more idiomatic
    - Update Kotlin Multiplatform Plugin to latest (2.1.0) and
      use languageSettings / stdlib dependencies to support
      targeting older Kotlin versions.
    - Clean up errors / warnings related to the fact we're now
      using the latest plugin.
    - Extract common logic (building / publishing / signing) to
      an internal build support plugin (no more `allProjects`!)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,6 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
-import org.jetbrains.kotlin.gradle.tasks.KotlinNativeCompile
-
 // Note: This seems to need to exist here and NOT in settings.gradle.kts pluginManagment, or else Gradle/Kotlin whines
 // at build about the same plugin being loaded multiple times.
 // See also: https://docs.gradle.org/current/userguide/plugins.html#sec:subprojects_plugins_dsl
-@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
@@ -24,25 +20,5 @@ nexusPublishing {
             (findProperty("ossrhToken") as? String)?.let { username.set(it) }
             (findProperty("ossrhTokenPassword") as? String)?.let { password.set(it) }
         }
-    }
-}
-
-allprojects {
-    tasks.withType<JavaCompile>().configureEach {
-        sourceCompatibility = JavaVersion.VERSION_11.toString()
-        targetCompatibility = JavaVersion.VERSION_11.toString()
-    }
-
-    tasks.withType<KotlinJvmCompile>().configureEach {
-        kotlinOptions.jvmTarget = "11"
-    }
-
-    tasks.withType<KotlinNativeCompile>().configureEach {
-        kotlinOptions.freeCompilerArgs += listOf(
-            "-opt-in=kotlin.RequiresOptIn",
-            // Posix APIs are experimental (but hopefully we're not using anything too controversial)
-            "-opt-in=kotlin.experimental.ExperimentalNativeApi",
-            "-opt-in=kotlinx.cinterop.ExperimentalForeignApi",
-        )
     }
 }
