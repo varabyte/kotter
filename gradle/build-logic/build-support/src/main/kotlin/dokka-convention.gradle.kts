5d9bb93e383a757ccd7c6f69d6fc9c0b5d8ed095
David Herman <bitspittle@gmail.com>
2026-07-29 11:39:08 -0700

    Tweak dokka output
    - Homepage link
    - Copyright footer
    - Updated rel path logic to be more friendly to isolated projects

--- gradle/build-logic/build-support/src/main/kotlin/dokka-convention.gradle.kts
+++ gradle/build-logic/build-support/src/main/kotlin/dokka-convention.gradle.kts
@@ -4,8 +4,16 @@ plugins {
 
 dokka {
     dokkaSourceSets.configureEach {
+        pluginsConfiguration.html {
+            homepageLink.set("https://github.com/varabyte/kotter")
+            footerMessage.set("Copyright © ${java.time.Year.now().value} Varabyte")
+        }
+
         sourceLink {
-            val path = project.projectDir.relativeTo(project.rootProject.projectDir).invariantSeparatorsPath
+            // Safe relative path resolution without accessing rootProject state
+            val rootDir = layout.settingsDirectory.asFile
+            val path = project.projectDir.relativeTo(rootDir).invariantSeparatorsPath
+
             localDirectory = project.projectDir.resolve("src")
             remoteUrl("https://github.com/varabyte/kotter/tree/main/$path/src")
             remoteLineSuffix = "#L"
